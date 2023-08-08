b92cf90b428eda66c28a948084cb34c6a62acf6f
raqim <mohd786raqim@gmail.com>
2023-08-07 17:55:39 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
-// vite.config.js
-// import { resolve } from 'path'
 // import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// // https://vitejs.dev/config/
 // export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//       },
-//     },
-//   },
+//   plugins: [react()],
 // })
+
+// vite.config.js;
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+});
