b3fb913ad65dbce4c21656078b72009d948371af
raqim <mohd786raqim@gmail.com>
2023-08-07 18:16:37 -0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
 // import {useState, useEffect} from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import EmpListing from "./components/EmpListing"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./components/EmpListing";
 import EmpEdit from "./components/EmpEdit";
 import EmpDetail from "./components/EmpDetail";
 import EmpCreate from "./components/EmpCreate";
@@ -9,25 +9,21 @@ import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 
 function App() {
-
   return (
-    <div className='App'>
-    <h1>React JS CRUD operations</h1>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<EmpListing/>}/>
-        <Route path='/edit/:id' element={<EmpEdit/>}/>
-        <Route path='/create' element={<EmpCreate/>}/>
-        <Route path='/detail/:id' element={<EmpDetail/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <h1>React JS CRUD operations</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<EmpListing />} />
+          <Route path="/edit/:id" element={<EmpEdit />} />
+          <Route path="/create" element={<EmpCreate />} />
+          <Route path="/detail/:id" element={<EmpDetail />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-
-
   );
- 
 }
 
-export default App
+export default App;
--- src/components/EmpListing.jsx
+++ src/components/EmpListing.jsx
@@ -1,40 +1,41 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 const EmpListing = () => {
   const [empdata, setEmpdata] = useState(null);
- 
-  const navigate= useNavigate()
 
-  const LoadEdit=(id)=>{
-navigate(`/edit/${id}`)
-  }
-  const RemoveFunction= (id)=>{
-    const conf= window.confirm('do you want to delete')
-    if(conf){
-  axios.delete(`https://users-data-chrj.onrender.com/data/${id}`)
-    .then((res) => {
-      alert('Record has been deleted')
-      navigate("/")
-      // setStatus(true)
-      console.log("dbwyedbwebdwehfvewgf",res)
-      // var resd = empdata.filter
-      
-    
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  }
-  }
-  const LoadDetail=(id)=>{
-    navigate(`/detail/${id}`)
-  }
+  const navigate = useNavigate();
+
+  const LoadEdit = (id) => {
+    navigate(`/edit/${id}`);
+  };
+  const RemoveFunction = (id) => {
+    const conf = window.confirm("do you want to delete");
+    if (conf) {
+      axios
+        .delete(`https://users-data-chrj.onrender.com/data/${id}`)
+        .then((res) => {
+          alert("Record has been deleted");
+          navigate("/");
+          // setStatus(true)
+          console.log("dbwyedbwebdwehfvewgf", res);
+          // var resd = empdata.filter
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  };
+  const LoadDetail = (id) => {
+    navigate(`/detail/${id}`);
+  };
   useEffect(() => {
-    axios.get("https://users-data-chrj.onrender.com/data")
+    axios
+      .get("https://users-data-chrj.onrender.com/data")
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data);
         setEmpdata(res.data);
       })
       .catch((err) => {
@@ -42,57 +43,93 @@ navigate(`/edit/${id}`)
       });
   }, []);
 
- 
   return (
     <div>
-    <Link  to ='/login/:id' className="btn btn-danger" >Login</Link>
-    <Link to='/signup/:id'className="btn btn-success bg-green">Sign Up</Link>
-    
-    <div className="conatiner">
-      <div className="card">
-        <div className="card-title">
-          <h1>Employee Data</h1>
-        </div>
-        <div className="card-body">
-          <div>
-            <Link to="/create" className="btn btn-success">
-              Add New (+)
-            </Link>
+      <Link to="/login/:id" className="btn btn-danger">
+        Login
+      </Link>
+      <Link to="/signup/:id" className="btn btn-success bg-green">
+        Sign Up
+      </Link>
+
+      <div className="conatiner">
+        <div className="card">
+          <div className="card-title">
+            <h1>Employee Data</h1>
           </div>
-          <table className="table table-bordered">
-            <thead>
-              <tr>
-                <td><strong>Id</strong></td>
-                <td><strong>Name</strong></td>
-                <td><strong>Email</strong></td>
-                <td><strong>Phone</strong></td>
-                <td><strong>Action</strong></td>
-              </tr>
-            </thead>
-            {
-              empdata?.length >0?<>
-              <tbody>
-              {empdata&&
-                empdata?.map((item) => (
-                  <tr key={item.id}>
-                    <td>{item.id}</td>
-                    <td>{item.name}</td>
-                    <td>{item.email}</td>
-                    <td>{item.phone}</td>
-                    <td>
-                      <a onClick={()=>{LoadEdit(item.id)}} className="btn btn-sm btn-success">Edit</a>
-                      <a onClick={()=>{RemoveFunction(item.id)}} className="btn btn-sm btn-danger">Remove</a>
-                      <a onClick={()=>{LoadDetail(item.id)}}className="btn btn-sm btn-primary">Details</a>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-              </>:<h1>No Data Found</h1>
-            }
-          </table>
+          <div className="card-body">
+            <div>
+              <Link to="/create" className="btn btn-success">
+                Add New (+)
+              </Link>
+            </div>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <td>
+                    <strong>Id</strong>
+                  </td>
+                  <td>
+                    <strong>Name</strong>
+                  </td>
+                  <td>
+                    <strong>Email</strong>
+                  </td>
+                  <td>
+                    <strong>Phone</strong>
+                  </td>
+                  <td>
+                    <strong>Action</strong>
+                  </td>
+                </tr>
+              </thead>
+              {empdata?.length > 0 ? (
+                <>
+                  <tbody>
+                    {empdata &&
+                      empdata?.map((item) => (
+                        <tr key={item.id}>
+                          <td>{item.id}</td>
+                          <td>{item.name}</td>
+                          <td>{item.email}</td>
+                          <td>{item.phone}</td>
+                          <td>
+                            <a
+                              onClick={() => {
+                                LoadEdit(item.id);
+                              }}
+                              className="btn btn-sm btn-success"
+                            >
+                              Edit
+                            </a>
+                            <a
+                              onClick={() => {
+                                RemoveFunction(item.id);
+                              }}
+                              className="btn btn-sm btn-danger"
+                            >
+                              Remove
+                            </a>
+                            <a
+                              onClick={() => {
+                                LoadDetail(item.id);
+                              }}
+                              className="btn btn-sm btn-primary"
+                            >
+                              Details
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </>
+              ) : (
+                <h1>No Data Found</h1>
+              )}
+            </table>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
